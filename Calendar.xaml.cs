19c43d464b4c8dc75326635cd48f822716715a69
paticzak <p.mundzik@gmail.com>
2016-06-09 23:24:05 +0200

    dodawanie eventu do kalendarza (nie działa)

--- Calendar.xaml.cs
+++ Calendar.xaml.cs
@@ -20,9 +20,41 @@ namespace KinomaniakInterfejsPart1wpf
     /// </summary>
     public partial class Calendar : UserControl
     {
+        public DateTime ChosenDay { get; set; }
         public Calendar()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        //wyświetlało komunikat do konkretnego dnia, tylko wiadomośc zawsze taka sama, zmienia się jedynie data
+        //private void Chosen_Day_Click(object sender, System.Windows.RoutedEventArgs e)
+        //{
+        //    string msg;
+ 
+        //    if (ChosenDay == null)
+        //        msg = "Choose a day.";
+        //    else
+        //        msg = string.Format("My favorite day is {0:D}",ChosenDay");
+ 
+        //    MessageBox.Show(msg);
+        //}
+
+        private void add_event_Click(object sender, RoutedEventArgs e)
+        {
+            EventModalWindow eventt = new EventModalWindow();
+            eventt.Occasion = new Event();
+            if(eventt.ShowDialog() == true)
+            {                 
+                Event happening = new Event();
+                if (ChosenDay != null)
+                {
+                    happening.eventName = string.Format("My favorite day is {0:D}", ChosenDay); // nic nie wyświetla
+                }
+                happening.eventName = eventt.Occasion.eventName;
+                happening.eventPlace = eventt.Occasion.eventPlace;
+                //eventList.Items.Add(happening); // wyrzuca błąd :(
+            }
         }
     }
 }
