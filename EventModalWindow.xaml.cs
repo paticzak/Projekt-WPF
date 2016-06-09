33d1eb2a855ccbda4365a2df9ff53531c3c491ea
paticzak <p.mundzik@gmail.com>
2016-06-09 23:18:28 +0200

    okienko, w którym dodajemy film

---
+++ EventModalWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KinomaniakInterfejsPart1wpf
+{
+    /// <summary>
+    /// Interaction logic for EventModalWindow.xaml
+    /// </summary>
+    public partial class EventModalWindow : Window
+    {
+        public EventModalWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Event Occasion { get; set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           // date.Text = Occasion.eventDate;
+            name.Text = Occasion.eventName;
+            place.Text = Occasion.eventPlace;
+
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            //Occasion.eventDate = date.Text;
+            Occasion.eventName = name.Text;
+            Occasion.eventPlace = place.Text;
+            DialogResult = true;
+            Close();
+
+        }
+
+   
+    }
+}
