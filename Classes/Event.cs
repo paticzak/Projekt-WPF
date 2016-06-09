e9f1c3a9763f25629c46ea44d4a783ea84e051bc
paticzak <p.mundzik@gmail.com>
2016-06-09 23:17:20 +0200

    Dane do dodawania eventu (data, miejsce, nazwa...)

---
+++ Classes/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinomaniakInterfejsPart1wpf
+{
+    public class Event
+    {
+        //public DateTime eventDate { get; set; }
+        public string eventName { get; set; }
+        public string eventPlace { get; set; }
+
+        
+        //public string eventPriority { get; set; } // grafika w postaci różnokolorowych gwiazdek ????
+
+        public override string ToString()
+        {
+            return eventName + " " + eventPlace;
+        }
+    }
+
+}
